1baaa27f199dd4c4115b055b0247612bef8b2c06
shatalovdm <shatalov.07@mail.ru>
2015-05-17 10:15:16 +0300

    changes in GUI

--- Students/src/graphic/AvgRate_GUI.java
+++ Students/src/graphic/AvgRate_GUI.java
@@ -12,46 +12,45 @@ import java.awt.event.WindowEvent;
 /**
  * Created by Дмитрий Шаталов on 5/9/2015.
  */
-public class AvgRate_GUI {
+public class AvgRate_GUI extends Frame {
     private Label headerLabel;
-    private Frame newFrame;
     private Panel controlPanel;
 
     public AvgRate_GUI() {
-        prepareFrame();
-    }
-    void prepareFrame() {
-        newFrame = new Frame("Students");
-        newFrame.setSize(400, 400);
-        newFrame.setLayout(new GridLayout(3, 1));
+        new Frame("Students");
+        setSize(400, 400);
+        setLayout(new GridLayout(3, 1));
         controlPanel = new Panel();
-        headerLabel = new Label();
-        newFrame.add(controlPanel);
-        newFrame.add(headerLabel);
-        newFrame.addWindowListener(new WindowAdapter() {
+        add(controlPanel);
+        addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
         });
-        newFrame.setVisible(true);
-    }
-    String loadFile() {
+        headerLabel = new Label();
         headerLabel.setText("Download a file with a list");
-        headerLabel.setLocation(100, 300);
+        //headerLabel.setLocation(100, 300);
         controlPanel.add(headerLabel);
-        FileDialog fileDialog = new FileDialog(newFrame,"Select a file", FileDialog.LOAD);
+        add(headerLabel);
+
         Button selectFile = new Button("Open file");
         selectFile.setBounds(50, 100, 70, 30);
         controlPanel.add(selectFile);
         selectFile.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                fileDialog.setVisible(true);
-                String fname = fileDialog.getDirectory() + fileDialog.getFile();
-                AvgRates ar = new AvgRates_HashMap();
-                headerLabel.setText(String.valueOf(ar.readData(fname).values().toArray()[0]));
+                loadFile();
             }
         });
+        setVisible(true);
+    }
+
+    String loadFile() {
+        FileDialog fileDialog = new FileDialog(this,"Select a file", FileDialog.LOAD);
+        fileDialog.setVisible(true);
+        String fname = fileDialog.getDirectory() + fileDialog.getFile();
+        AvgRates ar = new AvgRates_HashMap();
+        headerLabel.setText(String.valueOf(ar.readData(fname).values().toArray()[0]));
         return fileDialog.getFile();
     }
 
@@ -72,13 +71,7 @@ public class AvgRate_GUI {
         return studentId.getText();
     }
 
-
-
-
     public static void main(String args[]){
-        AvgRate_GUI f=new AvgRate_GUI();
-        f.loadFile();
-        f.makeAverage();
-
+        new AvgRate_GUI();
     }
 }
